e2bfc32579fc2619834cec317c647c8fdb61ffdf
Vitaly Minakov <mindev97@gmail.com>
2019-12-14 12:23:53 +0300

    initialize project

---
+++ votes-backend/src/main/java/ru/hackaton/vsu/votes/VotesApplication.java
@@ -0,0 +1,26 @@
+package ru.hackaton.vsu.votes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		VotesApplication.class,
+		Jsr310JpaConverters.class
+})
+public class VotesApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotesApplication.class, args);
+	}
+}
