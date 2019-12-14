d25f9d175a6a433af8669e2503066b717df0320e
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2019-12-14 15:58:53 +0300

    1.9.1
    ***Сервисочек***

--- src/main/java/ru/hackaton/vsu/votes/util/AppConstants.java
+++ src/main/java/ru/hackaton/vsu/votes/util/AppConstants.java
@@ -1,8 +1,7 @@
 package ru.hackaton.vsu.votes.util;
 
 public class AppConstants {
+    public static final int MAX_PAGE_SIZE = 50;
     String DEFAULT_PAGE_NUMBER = "0";
     String DEFAULT_PAGE_SIZE = "30";
-
-    int MAX_PAGE_SIZE = 50;
 }
