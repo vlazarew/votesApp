0521bc7d26886806b988e0095d6ddd36db0c6c8b
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2019-12-14 16:01:35 +0300

    1.9.2
    Поправил константы

--- src/main/java/ru/hackaton/vsu/votes/util/AppConstants.java
+++ src/main/java/ru/hackaton/vsu/votes/util/AppConstants.java
@@ -2,6 +2,6 @@ package ru.hackaton.vsu.votes.util;
 
 public class AppConstants {
     public static final int MAX_PAGE_SIZE = 50;
-    String DEFAULT_PAGE_NUMBER = "0";
-    String DEFAULT_PAGE_SIZE = "30";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "30";
 }
